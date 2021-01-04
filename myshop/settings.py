610ccc9a56dde44039f98a611fddc93ac39f198d
teefats <tee_fats@yahoo.com>
2021-01-04 21:05:08 +0000

    Added cart elements

--- myshop/settings.py
+++ myshop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
     
 ]
 
@@ -63,7 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+                'django.contrib.messages.context_processors.messages','cart.context_processors.cart',
             ],
         },
     },
@@ -123,3 +124,5 @@ STATIC_URL = '/static/'
 
 MEDIA_URL ='/media/'
 MEDIA_ROOT =os.path.join(BASE_DIR, 'media/')
+
+CART_SESSION_ID = 'cart'
